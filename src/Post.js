287a16ff3594756de036b793b63dfebb33e9ee8d
awantech-io <hello.wanlab@gmail.com>
2020-10-12 22:50:24 +0800

    header and post component

---
+++ src/Post.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Post() {
+    return (
+        <div className=''>
+            <h3>Username</h3> 
+           {/* header-> avatar + username */}
+
+           {/* image */}
+
+           {/* username + caption */}
+        </div>
+    )
+}
+
+export default Post
+
